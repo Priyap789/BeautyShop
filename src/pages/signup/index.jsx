fa466eeb11a63319732d8af8534005ec8ff144c7
priya <prajapati.priya789@gmail.com>
2025-12-19 16:30:32 +0530

    add space

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -3,7 +3,7 @@ import SignupForm from "../../components/SignupForm";
 export default function SignupPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-red-200">
-      <SignupForm />
+      <SignupForm  />
     </div>
   );
 }
